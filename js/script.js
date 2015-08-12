64010fecd1ca65e034b1fe7da53072b0d5ef9963
Taj <tajdar.afridi123@gmail.com>
2015-08-12 11:19:02 +0500

    Changes in js
    Changes in js file.

--- js/script.js
+++ js/script.js
@@ -22,18 +22,5 @@ offsetTop = offset.top;
 $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
 
-// Adding active class
-$(function(){
 
-var url = window.location.pathname, 
-	urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
-	// now grab every link from the navigation
-	$('#left-nav a').each(function(){
-		// and test its normalized href against the url pathname regexp
-		if(urlRegExp.test(this.href.replace(/\/$/,''))){
-			$(this).addClass('current');
-		}
-	});
-
-});
 
